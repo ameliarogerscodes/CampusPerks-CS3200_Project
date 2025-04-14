daf3133cb01142487a7686ce3dd3534d5f0be78f
ameliarogerscodes <rogers.ame@northeastern.edu>
2025-04-14 01:00:45 -0400

    Update stores_routes.py

--- api/backend/stores/stores_routes.py
+++ api/backend/stores/stores_routes.py
@@ -11,9 +11,43 @@ from backend.db_connection import db
 from backend.ml_models.model01 import predict
 
 stores = Blueprint('stores', __name__)
-
 (stores.route('/stores'))
+# get all stores
 def get_all_stores():
+    cursor = db.get_db().cursor()
+    the_query = '''
+        SELECT storeId,
+            name,
+            locationStreet,
+            locationCity,
+            locationState,
+            locationCountry,
+            priceRange,
+            noOfDiscounts,
+            hoursOfOperations,
+            category,
+            phoneNo,
+            website,
+            starRating,
+            delivery,
+            ageRestricted,
+            totalSales,
+            noOfOrders,
+            college
+        FROM stores
+    '''
+
+    cursor.execute(the_query)
+    theData = cursor.fetchall()
+    the_response = make_response(theData)
+    the_response.status_code = 200
+    the_response.mimetype='application/json'
+    return the_response
+
+# get store details for a specifc store
+@stores.route('/stores/<int:storeId>', methods=['GET'])
+def get_store_details(storeId):
+    current_app.logger.info('GET /stores/<int:storeId> route')
     cursor = db.get_db().cursor()
     the_query = '''
         SELECT storeId,
