323fb50a89ac34600d52296c53269c07979b20db
ameliarogerscodes <rogers.ame@northeastern.edu>
2025-04-14 00:31:06 -0400

    Update discount_routes.py
    get, put, delete routes for used discounts

--- api/backend/discounts/discount_routes.py
+++ api/backend/discounts/discount_routes.py
@@ -36,12 +36,10 @@ def get_discounts():
     return the_response
 
 
-
+# add a new discount to the DB
 @discounts.route('/discounts', methods=['POST'])
 def add_discount():
     current_app.logger.info('POST /discounts route')
-
-
     data = request.get_json()
 
     required_fields = ['storeId', 'code', 'percentOff', 'item', 'startDate',
@@ -81,3 +79,61 @@ def add_discount():
     except Exception as e:
         current_app.logger.error(f"Error inserting discount: {e}")
         return make_response(jsonify({'error': 'Failed to add discount'}), 500)
+
+
+# get a single discount by id
+@discounts.route('/discounts/<discountId>', methods=['GET'])
+def get_discount_by_id(discountId):
+    current_app.logger.info('GET /discounts/<discountId> route')
+    cursor = db.get_db().cursor()
+
+    query = '''
+        SELECT discountId, storeId, code, percentOff, item, startDate,
+               endDate, ageRestricted, minPurchase, bdayDiscount
+        FROM discount
+        WHERE discountId = %s
+    '''
+    cursor.execute(query, (discountId,))
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# update a discount by ID
+@discounts.route('/discounts/<int: discountId>', methods=['PUT'])
+def get_discount_by_id(discountId):
+    current_app.logger.info('PUT /discounts/<discountId> route')
+    data = request.get_json()
+    query = '''
+        UPDATE discount SET percentOff = %s, endDate = %s
+        WHERE discountId = %s
+    '''
+    values = (
+        data['percentOff'], 
+        data['endDate'],
+        discountId
+    )
+    cursor = db.get_db().cursor()
+    cursor.exceute(query,values)
+    db.get_db().commit()
+    return 'discount updated succesfully'
+
+# delete a discount by ID 
+@discounts.route('/discounts/<int: discountId>', methods=['DELETE'])
+def delete_discount(discountId):
+    current_app.logger.info('DELETE /discounts/<discountId> route')
+    query = '''
+        DELETE FROM discount WHERE discountId = %s
+    '''
+    cursor = db.get_db().cursor()
+    cursor.exceute(query, (discountId,))
+    db.get_db().commit()
+    return 'discount deleted succesfully'
+
+    
